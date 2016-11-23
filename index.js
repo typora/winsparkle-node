bde847dbc99ec07e3e7a698fbdc3479eb30701c1
edolganov <evgenij.dolganov@gmail.com>
2016-11-23 11:15:24 +0300

    Support of setLang in JS level

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var addon = require('./build/Release/addon.node');
 module.exports = {
   cleanup : addon.winSparkleCleanup,
   setAppcastUrl : addon.winSparkleSetAppcastUrl,
+  setLang : addon.winSparkleSetLang,
   checkUpdateWithUI : addon.winSparkleCheckUpdateWithUI,
   init : addon.winSparkleInit,
   setAutomaticCheckForUpdates : addon.winSparkleSetAutomaticCheckForUpdates,
